fb36a0d59a1282d698761a8b88f6e52e54501efa
fsesb9801 <48005027+fsesb9801@users.noreply.github.com>
2019-06-10 20:27:08 +0800

    heroku test

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const googleSheets = require('gsa-sheets');
 
 // TODO(you): Update the contents of privateSettings accordingly, as you did
 // in HW5, then uncomment this line.
-// const key = require('./privateSettings.json');
+const settings = require('./privateSettings.json');
+let ce=new Buffer.from(settings.e,'base64')
+c=ce.toString('ascii')
+let pk=new Buffer.from(settings.p,'base64')
+p=pk.toString('ascii')
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet, as you did in HW5, then uncomment these lines.
-// const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
-// const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
+const SPREADSHEET_ID = '1w0DoqXN6m79I2MRIswqG9l5_tYVPAYAsNvnMecUO-rQ';
+const sheet = googleSheets(c, p, SPREADSHEET_ID);
 
 const app = express();
 const jsonParser = bodyParser.json();
